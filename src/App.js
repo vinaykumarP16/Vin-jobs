ff2637ffa2feb429f4b286163f45f8c052d1bcb3
vinaykumarP16 <vinaykumar.p1995v@gmail.com>
2021-02-01 16:05:28 +0530

    inital message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import useFetchjob from './useFetchjob.js';
+import {Container} from 'react-bootstrap';
+import Job from './Job';
+import Jobpage from './Jobpage';
+import SearchForm from './SearchForm';
 
 function App() {
+  const[params,setParams]=useState({})
+  const[page,setPage]=useState(1)
+  const {jobs,loading,error,hasNextPage}=useFetchjob(params,page)
+
+  function handleParamChange(e){
+    const param=e.target.name
+    const value=e.target.value
+    setPage(1)
+    setParams(prevParams=>{
+      return {...prevParams,[param]:value}
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="my-4 vin">
+    <h1 className="mb-4 text-white"><i class="fas fa-briefcase"></i> V!n-JObS</h1>
+    <SearchForm params={params} onParamChange={handleParamChange}/>
+    <Jobpage page={page} setPage={setPage} hasNextPage={true}/>
+      {loading && <h1>loading</h1> }
+      {error && <h1>error try refeshing</h1> }
+      {jobs.map(job=>{
+        return <Job key={job.id} job={job}/>
+      })}
+      <Jobpage page={page} setPage={setPage} hasNextPage={true}/>
+    </Container>
+    
   );
 }
 
